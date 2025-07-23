f290c24310e04251c66cdd66089bfe7c3f57d442
YuvanX <abhivigneshofficial@gmail.com>
2025-07-23 21:59:43 +0530

    fix: db query

--- apps/web/app/api/signup/route.ts
+++ apps/web/app/api/signup/route.ts
@@ -2,6 +2,7 @@ import { signup } from "@/types/signupType";
 import { prisma } from "@workspace/db/client";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt"
+
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const { email, password, username } = body;
@@ -16,7 +17,7 @@ export async function POST(req: NextRequest) {
     const { success } = signup.safeParse({ email, password, username });
     if (!success) return NextResponse.json({ message: "Invalid Inputs" }, { status: 400 });
 
-    const doesUserExists = await prisma.user.findFirst({ where: email });
+    const doesUserExists = await prisma.user.findUnique({ where: { email } });
     if (doesUserExists) return NextResponse.json({ message: "Already user with this email exists." },{ status: 410 });
 
     const hashedPassword = await bcrypt.hash(password, 10)
@@ -27,6 +28,8 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json({ message: "User created Successfully!" });
   } catch (error) {
+    console.log(error);
+    
     return NextResponse.json("Something went wrong. Please try again later")
   }
 }
