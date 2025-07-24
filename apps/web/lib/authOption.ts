f61ebf0fb5ad66d05f495c0aaa5aea1762c6d324
YuvanX <abhivigneshofficial@gmail.com>
2025-07-24 13:39:50 +0530

    fix: session strategy

--- apps/web/lib/authOption.ts
+++ apps/web/lib/authOption.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@workspace/db/client";
 import bcrypt from "bcrypt";
+import { SessionStrategy } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
 export const authOptions = {
@@ -12,49 +13,53 @@ export const authOptions = {
       },
       async authorize(credentials, req) {
         try {
-                if(!credentials) {
-                    throw new Error("Please provide valid details")
-                }
+          if (!credentials) {
+            throw new Error("Please provide valid details");
+          }
 
-                const { email, password } = credentials;
+          const { email, password } = credentials;
 
-                const isUser = await prisma.user.findUnique({ where: { email }})
-                if(!isUser) {
-                    throw new Error("Invalid User. Please signup first!")
-                }
+          const isUser = await prisma.user.findUnique({ where: { email } });
+          if (!isUser) {
+            throw new Error("Invalid User. Please signup first!");
+          }
 
-                const hashedPassword = await bcrypt.hash(password, 10)
-                const isValidPassword = await bcrypt.compare(password, hashedPassword)
-                if(!isValidPassword) { 
-                    throw new Error("Invalid Password")
-                }
+          const isValidPassword = await bcrypt.compare(
+            password,
+            isUser.password
+          );
+          if (!isValidPassword) {
+            throw new Error("Invalid Password");
+          }
 
-                return { id: isUser.id, email: isUser.email }
-                
+          return { id: isUser.id, email: isUser.email };
         } catch (error) {
-
-            throw new Error("Something went wrong")
+          throw new Error("Something went wrong");
         }
       },
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET || "",
+  session: {
+    strategy: 'jwt' as SessionStrategy
+  },
   callbacks: {
     async jwt({ token, user }: any) {
-        if(token && user) {
-            token.id = user.id
-        }
+      if (token && user) {
+        token.id = user.id;
+      }
 
-        return token
+      return token;
     },
     async session({ session, token }: any) {
-        if(session && token) {
-            session.user.id = token.id
-        }
+      if (session && token) {
+        session.user.id = token.id;
+      }
 
-        return session
-    }
+      return session;
+    },
   },
   pages: {
-    signIn: "/signin"
-  }
+    signIn: "/signin",
+  },
 };
