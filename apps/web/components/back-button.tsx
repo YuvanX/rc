47d5b7cb8af6686dea37b26c129f3daf5fb60cef
YuvanX <abhivigneshofficial@gmail.com>
2025-07-25 22:30:21 +0530

    fix: Overlay layout

--- apps/web/components/back-button.tsx
+++ apps/web/components/back-button.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { Button } from "@workspace/ui/components/button"
 import { ArrowLeft } from "lucide-react"
+import { useRouter } from "next/navigation";
 
 export const BackButton = () => {
-    return <Button className="bg-[#BAFD50] cursor-pointer my-2">
+    const router = useRouter()
+
+    return <Button onClick={() => router.back()} className="bg-[#BAFD50] hover:bg-[#c8ee7b] cursor-pointer my-2">
         <ArrowLeft />
     </Button>
 }
